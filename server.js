3caad39d43630d79c211df4ca22781c9e2b9681d
Amir <amir@d207-023-206-110.wireless.sfu.ca>
2019-01-31 15:04:46 -0800

    fix alter street utf8

--- server.js
+++ server.js
@@ -114,7 +114,7 @@
   let set_var = ' SET @defualt := Null ';
   let err_handler_name = ' ALTER TABLE aml_pro_dev.info MODIFY COLUMN name Text CHARACTER SET utf8 COLLATE utf8_general_ci ';
   let err_handler_des = ' ALTER TABLE aml_pro_dev.info MODIFY COLUMN description Text CHARACTER SET utf8 COLLATE utf8_general_ci ';
-  let err_handler_des = ' ALTER TABLE aml_pro_dev.address MODIFY COLUMN street VARCHAR(255) CHARACTER SET utf8 COLLATE utf8_general_ci ';
+  let err_handler_street = ' ALTER TABLE aml_pro_dev.address MODIFY COLUMN street VARCHAR(255) CHARACTER SET utf8 COLLATE utf8_general_ci ';
   let db_a = new Database(db_config); 
   db_a.query(truncate_info)
   .then( rows => db_a.query(set_var))
