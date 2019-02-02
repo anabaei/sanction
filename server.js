b672fa4bb50f7dab5254c834e9e0b98b0b4bfbd0
Amir <amir@Mehdis-MBP.hitronhub.home>
2019-02-02 04:23:44 -0800

    add eu_eeas_sanctions table

--- server.js
+++ server.js
@@ -653,35 +653,29 @@
       //  db_db_1.query(coe_assembly_nationalitiescountry)
         db_db_1.query(err_handler_name)
         .then(rows => db_db_1.query(info_table))
-      
         .then( rows=> db_db_1.query(updateSanctionList))
-
-        .then( rows => db_db_1.query(ca_dfatd_sema_sanctions_aliases))
-        .then( rows => db_db_1.query(ca_dfatd_sema_sanctions_birth_dates))
-        .then( rows => db_db_1.query(ca_dfatd_sema_sanctions_nationalities))
-        .then( rows => db_db_1.query(eu_eeas_sanctions_identifiers))
-
-         .then( rows => db_db_1.query(au_dfat_address)) 
-       //   .then( rows => db.query(everypolitician_aliases)) 
+        .then( rows => db_db_1.query(au_dfat_address)) 
+       //.then( rows => db.query(everypolitician_aliases)) 
          .then( rows => db_db_1.query(au_dfat_sanctions_aliases)) 
          .then( rows => db_db_1.query(update_alias)) 
          .then( rows => db_db_1.query(birth_date)) 
          .then( rows => db_db_1.query(birth_place)) 
-      
      
        //  .then( rows => db_db.query(ch_seco_sanctions_addresses)) // HERE WE GET ERRORc= about utf8 fristName TODO
          .catch( err => {
           console.log("Err = "+ err);
       } )
 
+      .then( rows => db_db_1.query(ca_dfatd_sema_sanctions_aliases))
+      .then( rows => db_db_1.query(ca_dfatd_sema_sanctions_birth_dates))
+      .then( rows => db_db_1.query(ca_dfatd_sema_sanctions_nationalities))
+      .then( rows => db_db_1.query(eu_eeas_sanctions_identifiers))
       
-     
-      
-      // .then( rows => db_db_1.query(eu_eeas_sanctions_aliases))
-      // .then( rows => db_db_1.query(eu_eeas_sanctions_birth_dates))
-      // .then( rows => db_db_1.query(eu_eeas_sanctions_birth_places))
-      // .then( rows => db_db_1.query(eu_eeas_sanctions_nationalities))
-      // .then( rows => db_db_1.query(eu_eeas_sanctions_addresses))
+      .then( rows => db_db_1.query(eu_eeas_sanctions_aliases))
+      .then( rows => db_db_1.query(eu_eeas_sanctions_birth_dates))
+      .then( rows => db_db_1.query(eu_eeas_sanctions_birth_places))
+      .then( rows => db_db_1.query(eu_eeas_sanctions_nationalities))
+      .then( rows => db_db_1.query(eu_eeas_sanctions_addresses))
      
 
          .then( rows => db_db_1.query(ch_seco_sanctions_aliases))
